3dc13367387812d9710c603b4a3a44b6742658a3
teangsreyroth08 <138769876+teangsreyroth08@users.noreply.github.com>
2024-09-12 16:07:17 +0700

    change app.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,15 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';  // Ensure jest-dom is available
 
 test('renders learn react link', () => {
   render(<App />);
